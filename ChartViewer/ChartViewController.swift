c85dbf682f6951b6a6639f0bebf66a9683256c84
Yongle <yongle.qi@outlook.com>
2017-11-28 22:58:47 +0100

    Add second line to ChartView

--- ChartViewer/ChartViewController.swift
+++ ChartViewer/ChartViewController.swift
@@ -11,10 +11,10 @@ class ChartViewController: UIViewController {
         let labelSettings = ChartLabelSettings(font: ExamplesDefaults.labelFont)
         
         var readFormatter = DateFormatter()
-        readFormatter.dateFormat = "dd.MM.yyyy"
+        readFormatter.dateFormat = "dd.MM"
         
         var displayFormatter = DateFormatter()
-        displayFormatter.dateFormat = "dd.MM.yyyy"
+        displayFormatter.dateFormat = "dd.MM"
         
         let date = {(str: String) -> Date in
             return readFormatter.date(from: str)!
@@ -30,62 +30,78 @@ class ChartViewController: UIViewController {
             return calendar.date(from: components)!
         }
         
-        func filler(_ date: Date) -> ChartAxisValueDate {
+   /*     func filler(_ date: Date) -> ChartAxisValueDate {
             let filler = ChartAxisValueDate(date: date, formatter: displayFormatter)
             filler.hidden = true
             return filler
-        }
+        } */
         
-        let chartPoints = [
-            createChartPoint(dateStr: "01.10.2015", percent: 5, readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createChartPoint(dateStr: "04.10.2015", percent: 10, readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createChartPoint(dateStr: "05.10.2015", percent: 30, readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createChartPoint(dateStr: "06.10.2015", percent: 70, readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createChartPoint(dateStr: "08.10.2015", percent: 79, readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createChartPoint(dateStr: "10.10.2015", percent: 90, readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createChartPoint(dateStr: "12.10.2015", percent: 47, readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createChartPoint(dateStr: "14.10.2015", percent: 60, readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createChartPoint(dateStr: "15.10.2015", percent: 70, readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createChartPoint(dateStr: "16.10.2015", percent: 80, readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createChartPoint(dateStr: "19.10.2015", percent: 90, readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createChartPoint(dateStr: "21.10.2015", percent: 100, readFormatter: readFormatter, displayFormatter: displayFormatter)
+        let chartPoints1 = [
+            createChartPoint(dateStr: "01.10", percent: 5, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "04.10", percent: 10, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "05.10", percent: 30, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "06.10", percent: 70, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "08.10", percent: 79, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "10.10", percent: 90, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "12.10", percent: 47, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "14.10", percent: 60, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "15.10", percent: 70, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "16.10", percent: 80, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "19.10", percent: 90, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "21.10", percent: 100, readFormatter: readFormatter, displayFormatter: displayFormatter)
+        ]
+        
+        let chartPoints2 = [
+            createChartPoint(dateStr: "01.10", percent: 90, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "04.10", percent: 34, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "05.10", percent: 23, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "06.10", percent: 45, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "08.10", percent: 75, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "10.10", percent: 22, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "12.10", percent: 13, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "14.10", percent: 57, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "15.10", percent: 100, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "16.10", percent: 45, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "19.10", percent: 64, readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createChartPoint(dateStr: "21.10", percent: 15, readFormatter: readFormatter, displayFormatter: displayFormatter)
         ]
         
         let yValues = stride(from: 0, through: 100, by: 10).map {ChartAxisValuePercent($0, labelSettings: labelSettings)}
         
         let xValues = [
-            createDateAxisValue("01.10.2015", readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createDateAxisValue("03.10.2015", readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createDateAxisValue("05.10.2015", readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createDateAxisValue("07.10.2015", readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createDateAxisValue("09.10.2015", readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createDateAxisValue("11.10.2015", readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createDateAxisValue("13.10.2015", readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createDateAxisValue("15.10.2015", readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createDateAxisValue("17.10.2015", readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createDateAxisValue("19.10.2015", readFormatter: readFormatter, displayFormatter: displayFormatter),
-            createDateAxisValue("21.10.2015", readFormatter: readFormatter, displayFormatter: displayFormatter)
+            createDateAxisValue("01.10", readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createDateAxisValue("03.10", readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createDateAxisValue("05.10", readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createDateAxisValue("07.10", readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createDateAxisValue("09.10", readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createDateAxisValue("11.10", readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createDateAxisValue("13.10", readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createDateAxisValue("15.10", readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createDateAxisValue("17.10", readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createDateAxisValue("19.10", readFormatter: readFormatter, displayFormatter: displayFormatter),
+            createDateAxisValue("21.10", readFormatter: readFormatter, displayFormatter: displayFormatter)
         ]
         
-        let xModel = ChartAxisModel(axisValues: xValues, axisTitleLabel: ChartAxisLabel(text: "Axis title", settings: labelSettings))
-        let yModel = ChartAxisModel(axisValues: yValues, axisTitleLabel: ChartAxisLabel(text: "Axis title", settings: labelSettings.defaultVertical()))
+        let xModel = ChartAxisModel(axisValues: xValues, axisTitleLabel: ChartAxisLabel(text: "Date", settings: labelSettings))
+        let yModel = ChartAxisModel(axisValues: yValues, axisTitleLabel: ChartAxisLabel(text: "Progress", settings: labelSettings.defaultVertical()))
         let chartFrame = ExamplesDefaults.chartFrame(view.bounds)
         var chartSettings = ExamplesDefaults.chartSettingsWithPanZoom
         chartSettings.trailing = 20
         
         // Set a fixed (horizontal) scrollable area 2x than the original width, with zooming disabled.
-        chartSettings.zoomPan.maxZoomX = 2
-        chartSettings.zoomPan.minZoomX = 2
-        chartSettings.zoomPan.minZoomY = 1
-        chartSettings.zoomPan.maxZoomY = 1
+        chartSettings.zoomPan.maxZoomX = 1
+        chartSettings.zoomPan.minZoomX = 1
+        chartSettings.zoomPan.minZoomY = 0.5
+        chartSettings.zoomPan.maxZoomY = 2
         
         let coordsSpace = ChartCoordsSpaceLeftBottomSingleAxis(chartSettings: chartSettings, chartFrame: chartFrame, xModel: xModel, yModel: yModel)
         let (xAxisLayer, yAxisLayer, innerFrame) = (coordsSpace.xAxisLayer, coordsSpace.yAxisLayer, coordsSpace.chartInnerFrame)
         
-        let lineModel = ChartLineModel(chartPoints: chartPoints, lineColor: UIColor.red, lineWidth: 2, animDuration: 1, animDelay: 0)
-        
+        let lineModel1 = ChartLineModel(chartPoints: chartPoints1, lineColor: UIColor.blue, lineWidth: 3, animDuration: 5, animDelay: 0)
+        let lineModel2 = ChartLineModel(chartPoints: chartPoints2, lineColor: UIColor.red, lineWidth: 3, animDuration: 5, animDelay: 0)
+
         // delayInit parameter is needed by some layers for initial zoom level to work correctly. Setting it to true allows to trigger drawing of layer manually (in this case, after the chart is initialized). This obviously needs improvement. For now it's necessary.
-        let chartPointsLineLayer = ChartPointsLineLayer(xAxis: xAxisLayer.axis, yAxis: yAxisLayer.axis, lineModels: [lineModel], delayInit: true)
+        let chartPointsLineLayer = ChartPointsLineLayer(xAxis: xAxisLayer.axis, yAxis: yAxisLayer.axis, lineModels: [lineModel1,lineModel2], delayInit: true)
         
         let guidelinesLayerSettings = ChartGuideLinesLayerSettings(linesColor: UIColor.black, linesWidth: 0.3)
         let guidelinesLayer = ChartGuideLinesLayer(xAxisLayer: xAxisLayer, yAxisLayer: yAxisLayer, settings: guidelinesLayerSettings)
@@ -105,7 +121,7 @@ class ChartViewController: UIViewController {
         
         
         // Set scrollable area 2x than the original width, with zooming enabled. This can also be combined with e.g. minZoomX to allow only larger zooming levels.
-        //        chart.zoom(scaleX: 2, scaleY: 1, centerX: 0, centerY: 0)
+            chart.zoom(scaleX: 1, scaleY: 1, centerX: 0, centerY: 10)
         
         // Now that the chart is zoomed (either with minZoom setting or programmatic zooming), trigger drawing of the line layer. Important: This requires delayInit paramter in line layer to be set to true.
         chartPointsLineLayer.initScreenLines(chart)
